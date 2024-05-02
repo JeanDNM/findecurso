a95ec6625aba71ec325ce8238432f6c2d0ce5a52
jean <jeanneyra06@gmail.com>
2024-05-02 10:40:46 -0700

    Initial Commit

---
+++ proyectofindecurso/proyectofindecursoApp.swift
@@ -0,0 +1,17 @@
+//
+//  proyectofindecursoApp.swift
+//  proyectofindecurso
+//
+//  Created by Jeanpierre on 2/5/24.
+//
+
+import SwiftUI
+
+@main
+struct proyectofindecursoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
