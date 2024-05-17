e84d938ca15f34e1686a692d798626c33c7f8c04
jean <jeanneyra06@gmail.com>
2024-05-17 10:19:35 -0700

    probar

--- proyectofindecurso/proyectofindecursoApp.swift
+++ proyectofindecurso/proyectofindecursoApp.swift
@@ -6,12 +6,25 @@
 //
 
 import SwiftUI
-
+import FirebaseCore
+import GoogleSignIn
+//import GoogleMobileAds
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication,
+                     open url: URL,
+                     options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
+        return GIDSignIn.sharedInstance.handle(url)
+  }
+}
 @main
 struct proyectofindecursoApp: App {
+    var dB: Void = FirebaseApp.configure()
+    // Registramos antes que nada AppDelegate, para que primero se llame a FirebaseApp.configure()
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HolderView().environmentObject(AuthViewModel())
         }
     }
 }
